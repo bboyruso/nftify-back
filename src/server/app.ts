be5d1297666f8e54d25acea63ed0d9ff282fe46a
¨sergey¨ <bboyruso@hotmail.com>
2023-05-25 18:33:45 +0200

    Add pingController test

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import { generalError } from "./middlewares/errorMiddlewares.js";
+import { generalError, notFoundError } from "./middlewares/errorMiddlewares.js";
 import { pingController } from "./controllers/ping/pingController.js";
 
 const app = express();
@@ -19,5 +19,7 @@ app.use(express.json());
 app.get("/ping", pingController);
 
 app.use(generalError);
+
+app.use(notFoundError);
 
 export default app;
