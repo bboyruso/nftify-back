fc492694bda549cb3df07d4f44d55cb9f0925933
¨sergey¨ <bboyruso@hotmail.com>
2023-05-25 20:21:48 +0200

    Create mongoose data base connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
-import "./loadEnviroment.js";
+import "./loadEnvironment.js";
 import createDebug from "debug";
 import app from "./server/app.js";
+import chalk from "chalk";
+import mongoose from "mongoose";
 
 const debug = createDebug("nftify-api:root");
 
 const port = process.env.PORT ?? 4000;
+const mongodbConnection = process.env.MONGODB_CONNECTION;
 
 app.listen(port, () => {
   debug(`Server started at http://localhost:${port}`);
 });
+
+if (!mongodbConnection) {
+  debug(chalk.red("Missing environment variable"));
+  process.exit(1);
+}
+
+try {
+  await mongoose.connect(mongodbConnection);
+
+  debug(chalk.green("Connected to database"));
+} catch (error: unknown) {
+  debug(chalk.red(`Error connecting to database: ${(error as Error).message}`));
+}
